65201b936e4fcc50fd6cd91c5d114218401d0f8f
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-19 07:16:45 +0545

    initializing socket.io

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-//Set-ExecutionPolicy RemoteSigned –Scope Process
-//run it at the start of the project
 import * as dotenv from 'dotenv';
+import http from 'http';
+import jwt from 'jsonwebtoken';
+import { Message } from './Modals/message.js';
+import { Server } from 'socket.io';
 dotenv.config();
 
 import app from "./Routes/route.js"
 import Connect from "./Utils/Connection.js"
 
+let io;
 try {
     const db = await Connect();
-    app.listen(process.env.PORT, () => { //put it at the end of the file
+
+    // Create an HTTP server
+    const server = http.createServer(app);
+
+    // Initialize Socket.IO
+    io = new Server(server);
+
+    let userSocketMap = new Map(); // Map to store user ID to socket ID mapping
+
+    io.on('connection', (socket) => {
+        console.log('New client connected');
+
+        socket.on('login', (token) => {
+            try {
+                console.log('Token:', token);
+                const user = jwt.verify(token, process.env.JWTSecret);
+                const userId = user.id;
+                userSocketMap.set(socket.id, { socketId: socket.id, userId }); // Store user ID and socket ID
+            } catch (error) {
+                console.log('Error verifying JWT:', error);
+            }
+        });
+
+        socket.on('message', async (data) => {
+            const { receiverId, message } = data;
+            const receiverSocket = userSocketMap.get(receiverId);
+            const senderSocket = userSocketMap.get(socket.id);
+            if (receiverSocket) {
+                socket.to(receiverSocket.socketId).emit('message', message);
+
+                try {
+                    Message.create({
+                        message: message,
+                        sender: senderSocket.userId,
+                        receiver: receiverId
+                    });
+                } catch (error) {
+                    console.log('Error saving message to database:', error);
+                }
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+            // Remove user ID to socket ID mapping on disconnect
+            userSocketMap.delete(socket.id);
+        });
+    });
+
+    server.listen(process.env.PORT, () => {
         console.log(`listening on port ${process.env.PORT}`)
     })
 } catch (err) {
     console.log(err);
 }
+
+export { io };
