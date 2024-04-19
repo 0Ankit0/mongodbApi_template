a4c484e5e28bf5e1dc55fa4d98a90b1ede2bf346
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-19 07:17:01 +0545

    changestream added to watch for change

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import app from "./Routes/route.js"
-import Connect from "./Connection.js";
+import Connect from "./Utils/Connection.js"
+import { setupChangeStream } from './Utils/ChangeStream.js';
 try {
 
-    const result = await Connect();
+    const db = await Connect();
+    setupChangeStream(db);
     app.listen(process.env.PORT, () => { //put it at the end of the file
         console.log(`listening on port ${process.env.PORT}`)
     })
