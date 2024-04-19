2e8b2a7c17ff18d748ec9b453cbf3ef33d8e990e
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-19 07:16:52 +0545

    added trycatch

--- Routes/route.js
+++ Routes/route.js
@@ -11,7 +11,12 @@ var app = express();
 app.use(Cors());
 app.use(express.json());
 app.get("/", async (req, res) => {
-    res.send("Welcome to the home page");
+    try {
+
+        res.status(200).send("Welcome to the home page");
+    } catch (error) {
+        res.status(401).send(error.message);
+    }
 });
 app.use('/user', userRouter);
 
@@ -21,6 +26,5 @@ const uploadsDir = path.join(__dirname, '..', 'uploads');
 // Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(uploadsDir));
 app.use('/upload', protect, uploadRouter);
-
 app.use('/sendmail', protect, mailRouter);
 export default app;
